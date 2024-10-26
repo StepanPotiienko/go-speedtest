352dd7ae199e392253bfdc6b4c9500d942392e2e
Stepan Potiienko <kotnexjieb@protonmail.com>
2024-10-26 10:32:06 +0300

    added check for network connection afore checking speed, added install bash script for go-speedtest

--- install.sh
+++ install.sh
@@ -1,6 +1,44 @@
 #!/bin/bash
-echo "Thank you for installing Go Speedtool."
+
+GO_FILE="main.go"
+ALIAS_NAME="go-speedtest"
+
 echo "Installing shall take a moment..."
-chmod +x ./main.go
-export PATH=$PATH:$PWD
-$(which go) run "main.go"; exit $?
+
+# Detect Go installation path
+GO_BIN_PATH=$(go env GOBIN)
+if [[ -z "$GO_BIN_PATH" ]]; then
+    # If GOBIN is not set, use GOPATH/bin as fallback
+    GO_BIN_PATH=$(go env GOPATH)/bin
+fi
+
+# Check if Go binary path was found
+if [[ -z "$GO_BIN_PATH" ]]; then
+    echo "Could not detect Go installation path. Ensure Go is installed and configured correctly."
+    exit 1
+fi
+
+# Build the Go file
+go build -o "$GO_BIN_PATH/$ALIAS_NAME" "$GO_FILE"
+if [[ $? -ne 0 ]]; then
+    echo "Failed to build $GO_FILE."
+    exit 1
+fi
+
+# Add Go bin path to PATH if not already added
+if ! echo "$PATH" | grep -q "$GO_BIN_PATH"; then
+    echo "export PATH=\$PATH:$GO_BIN_PATH" >> "$HOME/.bashrc"
+    export PATH="$PATH:$GO_BIN_PATH"
+    echo "Added $GO_BIN_PATH to PATH in .bashrc"
+else
+    echo "$GO_BIN_PATH is already in PATH"
+fi
+
+if command -v "$ALIAS_NAME" &> /dev/null; then
+    echo "Command '$ALIAS_NAME' is ready to use. You can run it with: $ALIAS_NAME"
+    source ~/.bashrc
+else
+    echo "Something went wrong. Please check the script output for any issues."
+fi
+
+echo "Thank you for installing Go Speedtool."
--- main.go
+++ main.go
@@ -1,39 +1,85 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+	"os"
+
 	"github.com/showwin/speedtest-go/speedtest"
-  "fmt"
 )
 
-func main()  {
-  var speedTestClient = speedtest.New()
-  var serverID int 
+func ensureConnected() (connected bool) {
+	serverList := [...]string{
+		"http://clients3.google.com/generate_204",
+		"https://google.com",
+		"https://example.com",
+	}
 
-  serverList, _ := speedTestClient.FetchServers()
-  for _, s := range serverList {
-    fmt.Printf("%s\n", s)
-  }
+	errList := []error{}
 
-  fmt.Printf("Enter id of the server you want to connect to (or leave 0 for auto): ")
-  fmt.Scan(&serverID)
+	for index := range serverList {
+		server := serverList[index]
 
-  if serverID == 0 {
-    // Store it as a variable for future reference
-    var autoServerID string = serverList[0].ID[:5]
-    serverFetch, _ := speedTestClient.FetchServerByID(autoServerID)
+		_, err := http.Get(server)
+		if err != nil {
+			fmt.Printf("Could not connect to %s\n", server)
+			errList = append(errList, err)
+		}
+	}
 
-    fmt.Printf("Connecting to %s", serverFetch)
-  }
+	if len(errList) == len(serverList) {
+		return false
+	}
 
-  targets, _ := serverList.FindServer([]int{serverID})
-
-  fmt.Println("Wait while we do the magic...")
-  for _, s := range targets {
-    s.PingTest(nil)
-		s.DownloadTest()
-		s.UploadTest()
-		fmt.Printf("Latency: %s, Download Speed: %s, Upload Speed: %s\n", s.Latency, s.DLSpeed, s.ULSpeed)
-		s.Context.Reset() 
-  }
+	return true
 }
 
+func main() {
+	var (
+		speedTestClient = speedtest.New()
+		serverID        int
+	)
+
+	if !ensureConnected() {
+		fmt.Println(
+			"You are not connected to network. Try connecting to it first, before checking your speed again.",
+		)
+
+		os.Exit(1)
+	}
+
+	serverList, _ := speedTestClient.FetchServers()
+	for _, s := range serverList {
+		fmt.Printf("%s\n", s)
+	}
+
+	fmt.Printf(
+		"Enter id of the server you want to connect to (or leave 0 for auto): ",
+	)
+
+	fmt.Scan(&serverID)
+
+	if serverID == 0 {
+		// Store it as a variable for future reference
+		var autoServerID string = serverList[0].ID[:5]
+		serverFetch, _ := speedTestClient.FetchServerByID(autoServerID)
+
+		fmt.Printf("Connecting to %s\n", serverFetch)
+	}
+
+	targets, _ := serverList.FindServer([]int{serverID})
+
+	fmt.Println("Wait while we do the magic...")
+	for _, s := range targets {
+		s.PingTest(nil)
+		s.DownloadTest()
+		s.UploadTest()
+		fmt.Printf(
+			"Latency: %s, Download Speed: %s, Upload Speed: %s\n",
+			s.Latency,
+			s.DLSpeed,
+			s.ULSpeed,
+		)
+		s.Context.Reset()
+	}
+}
